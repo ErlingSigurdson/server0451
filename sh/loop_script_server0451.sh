034f32e1bb9526929549f7ddeb9ca7df35dba653
Erling Sigurdson <erlingsigurdson1@gmail.com>
2024-03-08 21:08:50 +0000

    Added check for bound port.

--- sh/loop_script_server0451.sh
+++ sh/loop_script_server0451.sh
@@ -3,13 +3,25 @@
 EXEC_FILEPATH="../bin/execute_server0451"
 CONFIG_FILEPATH="./configs_server0451"
 
-nohup bash -c \
-"while true
+PORT_1=80
+PORT_2=451
+
+while true
 do
-    sudo $EXEC_FILEPATH -p 80 -f $CONFIG_FILEPATH
+    sudo lsof -l -P -n > tmp_1
+    PORT_1_BOUND=$(grep -c ":$PORT_1.*LISTEN" tmp_1)
+    rm tmp_1
+    if [ $PORT_1_BOUND -eq 0 ]; then
+        sudo $EXEC_FILEPATH -p $PORT_1 -f $CONFIG_FILEPATH
+    fi
 done &
 
 while true
 do
-    sudo $EXEC_FILEPATH -p 451 -f $CONFIG_FILEPATH
-done &"
+    sudo lsof -l -P -n > tmp_2
+    PORT_2_BOUND=$(grep -c ":$PORT_2.*LISTEN" tmp_2)
+    rm tmp_2
+    if [ $PORT_2_BOUND -eq 0 ]; then
+        sudo $EXEC_FILEPATH -p $PORT_2 -f $CONFIG_FILEPATH
+    fi
+done &
--- sh/oneshot_script_server0451.sh
+++ sh/oneshot_script_server0451.sh
@@ -3,5 +3,19 @@
 EXEC_FILEPATH="../bin/execute_server0451"
 CONFIG_FILEPATH="./configs_server0451"
 
-sudo $EXEC_FILEPATH -p 80 -f $CONFIG_FILEPATH -v
-sudo $EXEC_FILEPATH -p 451 -f $CONFIG_FILEPATH -v
+PORT_1=80
+PORT_2=451
+
+sudo lsof -l -P -n > tmp_1
+PORT_1_BOUND=$(grep -c ":$PORT_1.*LISTEN" tmp_1)
+rm tmp_1
+if [ $PORT_1_BOUND -eq 0 ]; then
+    sudo $EXEC_FILEPATH -p $PORT_1 -f $CONFIG_FILEPATH -v
+fi
+    
+sudo lsof -l -P -n > tmp_2
+PORT_2_BOUND=$(grep -c ":$PORT_2.*LISTEN" tmp_2)
+rm tmp_2
+if [ $PORT_2_BOUND -eq 0 ]; then
+    sudo $EXEC_FILEPATH -p $PORT_2 -f $CONFIG_FILEPATH -v
+fi
