8050ad70de6a9edb65827d3e5c34f4637c24731b
Erling Sigurdson <erlingsigurdson1@gmail.com>
2024-04-23 23:21:15 +0000

    Minor corrections in shell scripts.

--- kill_server0451.sh
+++ kill_server0451.sh
@@ -4,10 +4,11 @@
 
 #--- Прочее ---#
 
+## Ограничение в 15 символов обусловлено ограничениями утилиты pkill.
 PATTERN_1="exec_bin_server"
 PATTERN_2="run_server0451"
 PATTERN_3="log_flush_serve"
-PATTERN_4="trace_script_se"
+PATTERN_4="trace_server_04"
 
 
 #******************* ПРОЦЕДУРЫ ******************#
